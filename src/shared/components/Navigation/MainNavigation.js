e2b513e9c56660080516e877e750dfc55d951845
Kamran Khan Alwi <beingkamrankhanalwi@gmail.com>
2023-08-11 19:14:22 +0530

    update

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -4,19 +4,30 @@ import {Link} from 'react-router-dom';
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    };
+
     return (
         <>
-            {drawerIsOpen ? (<SideDrawer>
+            {drawerIsOpen && <Backdrop onClick={closeDrawer}/>};
+            {drawerIsOpen && (<SideDrawer>
                 <nav className="main-navigation__drawer-nav">
                     <NavLinks />
                 </nav>
-            </SideDrawer>) : null }
+            </SideDrawer>)}
             <MainHeader>
-                <button className="main-navigation__menu-btn">
+                <button className="main-navigation__menu-btn" onClick={openDrawer}>
                     <sapn />
                     <sapn />
                     <sapn />
