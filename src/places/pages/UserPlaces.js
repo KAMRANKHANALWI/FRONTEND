bc6f7dbbba36e7f4486007f3eb763555e9a7c5c7
Kamran Khan Alwi <beingkamrankhanalwi@gmail.com>
2023-08-12 15:43:52 +0530

    added modal for map

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,42 +1,5 @@
-// import React from "react";
-
-// import PlaceList from "../components/PlaceList";
-
-// const DUMMY_PLACES = [
-//     {
-//         id: 'p1',
-//         title: 'Empire State Building',
-//         description: 'One of the most famous sky scrapers in the world',
-//         imageUrl : 'https://www.esbnyc.com/sites/default/files/styles/on_3_cols_free_height/public/2019-11/Screen%20Shot%202019-11-12%20at%2015.56.59.png?itok=YMjDhMKZ',
-//         address: '20 W 34th St., New York, NY 10001, United States',    
-//         location: {
-//             lat: 40.7480155,
-//             lgn: -73.9847686
-//         },
-//         creator: 'u1'
-//     },
-//     {
-//         id: 'p2',
-//         title: 'Empire State Building',
-//         description: 'One of the most famous sky scrapers in the world',
-//         imageUrl : 'https://www.esbnyc.com/sites/default/files/styles/on_3_cols_free_height/public/2019-11/Screen%20Shot%202019-11-12%20at%2015.56.59.png?itok=YMjDhMKZ',
-//         address: '20 W 34th St., New York, NY 10001, United States',    
-//         location: {
-//             lat: 40.7480155,
-//             lgn: -73.9847686
-//         },
-//         creator: 'u2'
-//     },
-// ]
-
-// const UserPlaces = () => {
-    
-//     return <PlaceList items={DUMMY_PLACES} />
-// };
-
-// export default UserPlaces;
-
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
@@ -68,8 +31,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
