dad6669050bf6deed3a63d1f6b36f431ed071d8b
Kamran Khan Alwi <beingkamrankhanalwi@gmail.com>
2023-08-13 21:01:59 +0530

    npm run build

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,8 +5,8 @@ const Users = () => {
     const USERS = [
         {
             id: 'u1', 
-            name: 'Zayn Malik', 
-            image: 'https://manilashaker.com/wp-content/uploads/2015/03/zayn-malik-confused-photo.jpg', 
+            name: 'Shawn Mendes', 
+            image: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F05%2Fsm-press-photo-brianziff-2017-2000.jpg', 
             places: 3
         }
     ];
